1b25f3ca041a4812c48bad6b4d1bd4d167806524
thiagosatake <thiago.seito.satake@gmail.com>
2024-06-12 16:16:22 -0300

    Initial commit

---
+++ src/test/java/com/example/demo/SecurityRestKeycloakSampleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class SecurityRestKeycloakSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
