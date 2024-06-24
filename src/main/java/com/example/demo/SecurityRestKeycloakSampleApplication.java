fc69b9a21eb1634ac0bfb485640af69c351474ed
thiago <thiago@debian>
2024-06-24 18:15:14 -0300

    Package refactor for web security

--- src/main/java/com/example/demo/SecurityRestKeycloakSampleApplication.java
+++ src/main/java/com/example/demo/SecurityRestKeycloakSampleApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import arqref.one.web.EnableArqRefOneWebSecurity;
+import arqref.one.web.security.EnableArqRefOneWebSecurity;
 
 @EnableArqRefOneWebSecurity
 @SpringBootApplication
